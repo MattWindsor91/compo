21430c9c4cb9f4538372a5c2923f5319457a1368
Matt Windsor <mbw500@york.ac.uk>
2013-12-26 04:24:29 +0000

    Fully specify Composite.

--- spec/composite_spec.rb
+++ spec/composite_spec.rb
@@ -54,37 +54,182 @@ describe MockComposite do
   end
 
   describe '#remove' do
-    before(:each) { allow(subject).to receive(:remove!) }
+    context 'when #remove! is defined' do
+      before(:each) { allow(subject).to receive(:remove!) }
 
-    context 'when #remove! returns nil' do
-      before(:each) { allow(subject).to receive(:remove!).and_return(nil) }
+      context 'when #remove! returns nil' do
+        before(:each) { allow(subject).to receive(:remove!).and_return(nil) }
 
-      specify { expect(subject.remove(child)).to be_nil }
+        specify { expect(subject.remove(child)).to be_nil }
 
-      it 'calls #remove! with the child given' do
-        expect(subject).to receive(:remove!).once.with(child)
-        subject.remove(child)
+        it 'calls #remove! with the child given' do
+          expect(subject).to receive(:remove!).once.with(child)
+          subject.remove(child)
+        end
+      end
+
+      context 'when #remove! returns the child' do
+        before(:each) do
+          allow(subject).to receive(:remove!).and_return(child)
+          allow(child).to receive(:remove_parent)
+        end
+
+        it 'calls #remove! with the child given' do
+          expect(subject).to receive(:remove!).once.with(child)
+          subject.remove(child)
+        end
+
+        it 'calls #remove_parent on the child with no arguments' do
+          expect(child).to receive(:remove_parent).once.with(no_args)
+          subject.remove(child)
+        end
+
+        it 'returns the given child' do
+          expect(subject.remove(child)).to eq(child)
+        end
       end
     end
 
-    context 'when #remove! returns the child' do
+    context 'when #remove_id! is defined but #remove! is not' do
       before(:each) do
-        allow(subject).to receive(:remove!).and_return(child)
-        allow(child).to receive(:remove_parent)
+        allow(subject).to receive(:remove_id!)
+        allow(subject).to receive(:children).and_return(id => child)
       end
 
-      it 'calls #remove! with the and child given' do
-        expect(subject).to receive(:remove!).once.with(child)
-        subject.remove(child)
+      context 'when #remove_id! returns nil' do
+        before(:each) do
+          allow(subject).to receive(:remove_id!).and_return(nil)
+        end
+
+        specify { expect(subject.remove(child)).to be_nil }
+
+        it 'calls #remove! with the child given' do
+          expect(subject).to receive(:remove!).once.with(child)
+          subject.remove(child)
+        end
+
+        it 'calls #remove_id! with the ID of the child' do
+          expect(subject).to receive(:remove_id!).once.with(id)
+          subject.remove(child)
+        end
+
+        it 'calls #children' do
+          expect(subject).to receive(:children).once
+          subject.remove(child)
+        end
       end
 
-      it 'calls #remove_parent on the child with no arguments' do
-        expect(child).to receive(:remove_parent).once.with(no_args)
-        subject.remove(child)
+      context 'when #remove_id! returns the child' do
+        before(:each) do
+          allow(subject).to receive(:remove_id!).and_return(child)
+          allow(child).to receive(:remove_parent)
+        end
+
+        it 'calls #remove_id! with the child given' do
+          expect(subject).to receive(:remove!).once.with(child)
+          subject.remove(child)
+        end
+
+        it 'calls #remove_parent on the child with no arguments' do
+          expect(child).to receive(:remove_parent).once.with(no_args)
+          subject.remove(child)
+        end
+
+        it 'returns the given child' do
+          expect(subject.remove(child)).to eq(child)
+        end
+      end
+    end
+  end
+
+  describe '#remove_id' do
+    let(:id) { double(:id) }
+
+    context 'when #remove_id! is defined' do
+      before(:each) { allow(subject).to receive(:remove_id!) }
+
+      context 'and #remove_id! returns nil' do
+        before(:each) do
+          allow(subject).to receive(:remove_id!).and_return(nil)
+        end
+
+        specify { expect(subject.remove_id(id)).to be_nil }
+
+        it 'calls #remove_id! with the ID given' do
+          expect(subject).to receive(:remove_id!).once.with(id)
+          subject.remove_id(id)
+        end
       end
 
-      it 'returns the given child' do
-        expect(subject.remove(child)).to eq(child)
+      context 'and #remove_id! returns the child' do
+        before(:each) do
+          allow(subject).to receive(:remove_id!).and_return(child)
+          allow(child).to receive(:remove_parent)
+        end
+
+        it 'calls #remove_id! with the ID given' do
+          expect(subject).to receive(:remove_id!).once.with(id)
+          subject.remove_id(id)
+        end
+
+        it 'calls #remove_parent on the child with no arguments' do
+          expect(child).to receive(:remove_parent).once.with(no_args)
+          subject.remove_id(id)
+        end
+
+        it 'returns the child' do
+          expect(subject.remove_id(id)).to eq(child)
+        end
+      end
+    end
+
+    context 'when #remove! is defined but #remove_id! is not' do
+      before(:each) { allow(subject).to receive(:remove!) }
+
+      context 'and #remove! returns nil' do
+        before(:each) do
+          allow(subject).to receive(:remove!).and_return(nil)
+          allow(subject).to receive(:get_child).and_return(child)
+        end
+
+        specify { expect(subject.remove_id(id)).to be_nil }
+
+        it 'calls #remove_id! with the ID given' do
+          expect(subject).to receive(:remove_id!).once.with(id)
+          subject.remove_id(id)
+        end
+
+        it 'calls #get_child with the ID given' do
+          expect(subject).to receive(:get_child).once.with(id)
+          subject.remove_id(id)
+        end
+
+        it 'calls #remove! with the child given' do
+          expect(subject).to receive(:remove!).once.with(child)
+          subject.remove_id(id)
+        end
+      end
+
+      context 'and #remove! returns the child' do
+        before(:each) do
+          allow(subject).to receive(:remove!).and_return(child)
+          allow(subject).to receive(:get_child).and_return(child)
+          allow(child).to receive(:remove_parent)
+        end
+
+        it 'calls #remove! with the child given' do
+          expect(subject).to receive(:remove!).once.with(child)
+          subject.remove_id(id)
+        end
+
+        it 'calls #remove_parent on the child with no arguments' do
+          expect(child).to receive(:remove_parent).once.with(no_args)
+          subject.remove_id(id)
+        end
+
+        it 'returns the given child' do
+          expect(subject.remove_id(id)).to eq(child)
+        end
       end
     end
   end
