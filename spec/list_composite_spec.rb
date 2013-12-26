8eb66cf4660f9daf7c013355bf59b898b16d07ad
Matt Windsor <mbw500@york.ac.uk>
2013-12-26 16:36:34 +0000

    Fill out specification for ListComposite.

--- spec/list_composite_spec.rb
+++ spec/list_composite_spec.rb
@@ -20,6 +20,19 @@ describe Compo::ListComposite do
   describe '#add' do
     context 'when the ID is not Numeric' do
       specify { expect(subject.add(:mr_flibble, child1)).to be_nil }
+
+      it 'does not add to the list of children' do
+          subject.add(:rimmer, child1)
+          expect(subject.children).to eq({})
+
+          subject.add(0, child1)
+          subject.add(:lister, child2)
+          expect(subject.children).to eq(0 => child1)
+
+          subject.add(1, child2)
+          subject.add(:cat, child3)
+          expect(subject.children).to eq(0 => child1, 1 => child2)
+        end
     end
     context 'when the ID is Numeric' do
       context 'and is equal to the number of children' do
@@ -60,7 +73,7 @@ describe Compo::ListComposite do
           expect(subject.add(3, child3)).to be_nil
         end
 
-        it 'does not to the list of children' do
+        it 'does not add to the list of children' do
           subject.add(1, child1)
           expect(subject.children).to eq({})
 
@@ -90,14 +103,95 @@ describe Compo::ListComposite do
           subject.add(1, child3)
           expect(subject.children).to eq(0 => child2, 1 => child3, 2 => child1)
         end
+
+        it 'calls #update_parent on the child with itself and an ID proc' do
+          expect(child1).to receive(:update_parent) do |parent, proc|
+            expect(parent).to eq(subject)
+            expect(proc.call).to eq(0)
+          end
+          subject.add(0, child2)
+          subject.add(0, child1)
+        end
       end
     end
   end
 
   describe '#remove' do
+    context 'when the child exists in the list' do
+      before(:each) { subject.add(0, child1) }
+
+      it 'returns the child' do
+        expect(subject.remove(child1)).to eq(child1)
+      end
+
+      it 'calls #remove_parent on the child' do
+        expect(child1).to receive(:remove_parent).once.with(no_args)
+        subject.remove(child1)
+      end
+
+      it 'moves succeeding IDs down by one' do
+        subject.add(1, child2)
+        subject.add(2, child3)
+        expect(subject.children).to eq(0 => child1, 1 => child2, 2 => child3)
+        subject.remove(child2)
+        expect(subject.children).to eq(0 => child1, 1 => child3)
+      end
+    end
+
+    context 'when the child does not exist in the list' do
+      specify { expect(subject.remove(child1)).to be_nil }
+
+      it 'does not change the children' do
+        expect(subject.children).to eq({})
+        subject.remove(child1)
+        expect(subject.children).to eq({})
+
+        subject.add(0, child1)
+        subject.add(1, child2)
+        expect(subject.children).to eq(0 => child1, 1 => child2)
+        subject.remove(child3)
+        expect(subject.children).to eq(0 => child1, 1 => child2)
+      end
+    end
   end
 
   describe '#remove_id' do
+    context 'when the ID exists' do
+      before(:each) { subject.add(0, child1) }
+
+      it 'returns the child' do
+        expect(subject.remove_id(0)).to eq(child1)
+      end
+
+      it 'calls #remove_parent on the child' do
+        expect(child1).to receive(:remove_parent).once.with(no_args)
+        subject.remove_id(0)
+      end
+
+      it 'moves succeeding IDs down by one' do
+        subject.add(1, child2)
+        subject.add(2, child3)
+        expect(subject.children).to eq(0 => child1, 1 => child2, 2 => child3)
+        subject.remove_id(1)
+        expect(subject.children).to eq(0 => child1, 1 => child3)
+      end
+    end
+
+    context 'when the ID does not exist' do
+      specify { expect(subject.remove_id(0)).to be_nil }
+
+      it 'does not change the children' do
+        expect(subject.children).to eq({})
+        subject.remove_id(0)
+        expect(subject.children).to eq({})
+
+        subject.add(0, child1)
+        subject.add(1, child2)
+        expect(subject.children).to eq(0 => child1, 1 => child2)
+        subject.remove_id(2)
+        expect(subject.children).to eq(0 => child1, 1 => child2)
+      end
+    end
   end
 
   describe '#children' do
