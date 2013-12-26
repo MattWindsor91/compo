e0e7d6871dc23e608786d6b57948c7ab05104c9a
Matt Windsor <mbw500@york.ac.uk>
2013-12-26 03:41:48 +0000

    Specify #add.

--- spec/composite_spec.rb
+++ spec/composite_spec.rb
@@ -12,9 +12,44 @@ describe MockComposite do
   describe '#add' do
     before(:each) { allow(subject).to receive(:add!) }
 
-    it 'calls #add! with the ID and child given' do
-      expect(subject).to receive(:add!).once.with(id, child)
-      subject.add(id, child)
+    context 'when #add! returns nil' do
+      before(:each) { allow(subject).to receive(:add!).and_return(nil) }
+
+      specify { expect(subject.add(id, child)).to be_nil }
+
+      it 'calls #add! with the ID and child given' do
+        expect(subject).to receive(:add!).once.with(id, child)
+        subject.add(id, child)
+      end
+    end
+
+    context 'when #add! returns the child' do
+      before(:each) do
+        allow(subject).to receive(:add!).and_return(child)
+        allow(subject).to receive(:id_function)
+        allow(child).to receive(:update_parent)
+      end
+
+      it 'calls #add! with the ID and child given' do
+        expect(subject).to receive(:add!).once.with(id, child)
+        subject.add(id, child)
+      end
+
+      it 'calls #id_function with the child given' do
+        expect(subject).to receive(:id_function).once.with(child)
+        subject.add(id, child)
+      end
+
+      it 'calls #update_parent on the child with the parent and ID function' do
+        idf = double(:id_function)
+        allow(subject).to receive(:id_function).and_return(idf)
+        expect(child).to receive(:update_parent).once.with(subject, idf)
+        subject.add(id, child)
+      end
+
+      it 'returns the given child' do
+        expect(subject.add(id, child)).to eq(child)
+      end
     end
   end
 end
