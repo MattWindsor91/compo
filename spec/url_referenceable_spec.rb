5c2f21da7979b7c147254436a80e658d92613af5
Matt Windsor <mbw500@york.ac.uk>
2013-12-28 21:35:12 +0000

    Add parent mocking.

--- spec/url_referenceable_spec.rb
+++ spec/url_referenceable_spec.rb
@@ -7,19 +7,25 @@ class MockUrlReferenceable
 end
 
 describe MockUrlReferenceable do
+  before(:each) { allow(subject).to receive(:parent).and_return(parent) }
+
   describe '#url' do
     context 'when the UrlReferenceable has no parent' do
+      let(:parent) { nil }
     end
 
     context 'when the UrlReferenceable has a parent' do
+      let(:parent) { double(:parent) }
     end
   end
 
   describe '#parent_url' do
     context 'when the UrlReferenceable has no parent' do
+      let(:parent) { nil }
     end
 
     context 'when the UrlReferenceable has a parent' do
+      let(:parent) { double(:parent) }
     end
   end
 end
