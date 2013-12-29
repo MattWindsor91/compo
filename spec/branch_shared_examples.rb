0671e5ebd34f0eb0747ac60a0a5db390053f6a47
Matt Windsor <mbw500@york.ac.uk>
2013-12-29 19:20:09 +0000

    Move URLReferenceable specs into SEs/Branch too

--- spec/branch_shared_examples.rb
+++ spec/branch_shared_examples.rb
@@ -1,4 +1,8 @@
+require 'url_referenceable_shared_examples'
+
 shared_examples 'a branch' do
+  it_behaves_like 'a URL referenceable object'
+
   describe '#initialize' do
     it 'initialises with a Parentless as parent' do
       expect(subject.parent).to be_a(Compo::Parentless)
--- spec/url_referenceable_spec.rb
+++ spec/url_referenceable_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'compo'
+require 'url_referenceable_shared_examples'
 
 # Mock implementation of UrlReferenceable.
 class MockUrlReferenceable
@@ -7,80 +8,5 @@ class MockUrlReferenceable
 end
 
 describe MockUrlReferenceable do
-  let(:parent) { nil }
-  let(:id) { double(:id) }
-
-  before(:each) do
-    allow(subject).to receive(:id).and_return(id)
-    allow(subject).to receive(:parent).and_return(parent)
-  end
-
-  describe '#url' do
-    context 'when the UrlReferenceable has no parent' do
-      specify { expect { subject.url }.to raise_error }
-    end
-
-    context 'when the UrlReferenceable has a parent' do
-      let(:parent) { double(:parent) }
-      before(:each) do
-        allow(parent).to receive(:child_url).and_return(:child_url)
-      end
-
-      it 'calls #id' do
-        expect(subject).to receive(:id)
-        subject.url
-      end
-
-      it 'calls #parent' do
-        expect(subject).to receive(:parent)
-        subject.url
-      end
-
-      it 'calls #child_url on the parent with the ID' do
-        expect(parent).to receive(:child_url).with(id)
-        subject.url
-      end
-
-      it 'returns the result of calling #child_url' do
-        expect(subject.url).to eq(:child_url)
-      end
-    end
-  end
-
-  describe '#parent_url' do
-    context 'when the UrlReferenceable has no parent' do
-      let(:parent) { nil }
-
-      specify { expect(subject.parent_url).to be_nil }
-
-      it 'calls #parent' do
-        expect(subject).to receive(:parent)
-        subject.parent_url
-      end
-    end
-
-    context 'when the UrlReferenceable has a parent' do
-      let(:parent) { double(:parent) }
-      let(:url) { double(:url) }
-
-      before(:each) do
-        allow(parent).to receive(:url).and_return(url)
-      end
-
-      it 'calls #parent' do
-        expect(subject).to receive(:parent).with(no_args)
-        subject.parent_url
-      end
-
-      it 'calls #url on the parent' do
-        expect(parent).to receive(:url).once.with(no_args)
-        subject.parent_url
-      end
-
-      it 'returns the result of #url on the parent' do
-        expect(subject.parent_url).to eq(url)
-        subject.parent_url
-      end
-    end
-  end
+  it_behaves_like 'a URL referenceable object'
 end
