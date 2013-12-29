91047e00d7a674cba5ae2b4183b3e08eff19a370
Matt Windsor <mbw500@york.ac.uk>
2013-12-29 19:11:30 +0000

    Actually add the &^#$# file...

---
+++ spec/branch_shared_examples.rb
@@ -0,0 +1,11 @@
+shared_examples 'a branch' do
+  describe '#initialize' do
+    it 'initialises with a Parentless as parent' do
+      expect(subject.parent).to be_a(Compo::Parentless)
+    end
+
+    it 'initialises with an ID function returning nil' do
+      expect(subject.id).to be_nil
+    end
+  end
+end
