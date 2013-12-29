bbd4a6bed74becca2f284bea1aefe0e6c6ef806a
Matt Windsor <mbw500@york.ac.uk>
2013-12-29 03:28:38 +0000

    Create specs for url and child_url.

--- spec/parentless_spec.rb
+++ spec/parentless_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 require 'compo'
 
 describe Compo::Parentless do
+  describe '#url' do
+    specify { expect(subject.url).to eq('') }
+  end
+
+  describe '#child_url' do
+    specify { expect(subject.child_url(:id)).to eq('') }
+  end
 end
