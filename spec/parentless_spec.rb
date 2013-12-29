de17bcd304d41a3401313f8e35d8373996c3581a
Matt Windsor <mbw500@york.ac.uk>
2013-12-29 03:34:38 +0000

    Specify #add! and #remove!

--- spec/parentless_spec.rb
+++ spec/parentless_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 require 'compo'
 
 describe Compo::Parentless do
+  let(:child) { double(:child) }
+
+  describe '#add!' do
+    it 'returns the given child exactly' do
+      expect(subject.add!(:id, child)).to be(child)
+    end
+  end
+
+  describe '#remove!' do
+    it 'returns the given child exactly' do
+      expect(subject.remove!(child)).to be(child)
+    end
+  end
+
   describe '#children' do
     specify { expect(subject.children).to eq({}) }
   end
