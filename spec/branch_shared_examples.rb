a284ef4f2e31437f089aca102a6bafbaf672ce38
Matt Windsor <mbw500@york.ac.uk>
2013-12-29 19:21:15 +0000

    Move Leaf URL examples to Branch.

--- spec/branch_shared_examples.rb
+++ spec/branch_shared_examples.rb
@@ -12,4 +12,20 @@ shared_examples 'a branch' do
       expect(subject.id).to be_nil
     end
   end
+
+  describe '#url' do
+    context 'when the Leaf has no parent' do
+      it 'returns the empty string' do
+        expect(subject.url).to eq('')
+      end
+    end
+    context 'when the Leaf is the child of a root' do
+      let(:parent) { Compo::HashBranch.new }
+      before(:each) { subject.move_to(parent, :id) }
+
+      it 'returns /ID, where ID is the ID of the Leaf' do
+        expect(subject.url).to eq('/id')
+      end
+    end
+  end
 end
--- spec/leaf_spec.rb
+++ spec/leaf_spec.rb
@@ -4,20 +4,4 @@ require 'branch_shared_examples'
 
 describe Compo::Leaf do
   it_behaves_like 'a branch'
-
-  describe '#url' do
-    context 'when the Leaf has no parent' do
-      it 'returns the empty string' do
-        expect(subject.url).to eq('')
-      end
-    end
-    context 'when the Leaf is the child of a root' do
-      let(:parent) { Compo::HashBranch.new }
-      before(:each) { subject.move_to(parent, :id) }
-
-      it 'returns /ID, where ID is the ID of the Leaf' do
-        expect(subject.url).to eq('/id')
-      end
-    end
-  end
 end
