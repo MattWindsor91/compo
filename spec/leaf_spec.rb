60e3f4ef10914006972abcf2a16e3c15645f4326
Matt Windsor <mbw500@york.ac.uk>
2013-12-29 19:26:09 +0000

    Make NullComposite SEs and add to Leaf

--- spec/leaf_spec.rb
+++ spec/leaf_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 require 'compo'
 require 'branch_shared_examples'
+require 'null_composite_shared_examples'
 
 describe Compo::Leaf do
   it_behaves_like 'a branch'
+  it_behaves_like 'a null composite'
 end
