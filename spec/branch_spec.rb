7eb73d0838c00f5b1ad74029a1f37338765db2f0
Matt Windsor <mbw500@york.ac.uk>
2014-05-21 16:19:49 +0100

    Add spec for mocked-up branches.

---
+++ spec/branch_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'compo'
+require 'branch_shared_examples'
+
+# Mock implementation of a Branch
+class MockBranch
+  include Compo::Branch
+end
+
+describe MockBranch do
+  it_behaves_like 'a branch'
+end
