f1e24c6f44f4b1ad07824cedd21f8fbb479c08e6
Matt Windsor <mbw500@york.ac.uk>
2014-05-22 11:57:23 +0100

    Add Branch#find_url.

--- lib/compo/branches/branch.rb
+++ lib/compo/branches/branch.rb
@@ -1,4 +1,5 @@
 require 'compo/mixins'
+require 'compo/finders'
 
 module Compo
   module Branches
@@ -12,6 +13,23 @@ module Compo
       include Mixins::Movable
       include Mixins::ParentTracker
       include Mixins::UrlReferenceable
+
+      # Traverses this Branch and its children following a URL
+      #
+      # See Compo::Finders::Url for more information about what this means.
+      # This is a convenience wrapper over that method object, and is equivalent
+      # to 'Compo::Finders::Url.find(self, *args, &block)'.
+      #
+      # @api  public
+      # @example  From this Branch, find the item at 'a/b/1'.
+      #   branch.on_url('a/b/1') { |item| p item }
+      #
+      # @yieldparam [Object]  The found resource.
+      #
+      # @return [Object]  Whatever is returned by the block.
+      def find_url(*args, &block)
+        Compo::Finders::Url.find(self, *args, &block)
+      end
     end
   end
 end
