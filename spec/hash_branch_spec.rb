7909e0e055bde558d18ad7ea73f75584ab64013d
Matt Windsor <mbw500@york.ac.uk>
2014-05-22 12:46:27 +0100

    Make rubocop happy.

--- spec/hash_branch_spec.rb
+++ spec/hash_branch_spec.rb
@@ -5,7 +5,7 @@ require 'hash_composite_shared_examples'
 
 describe Compo::Branches::Hash do
   it_behaves_like 'a branch with children' do
-    let(:initial_ids) { ['a', 'b'] }
+    let(:initial_ids) { %w(a b) }
   end
   it_behaves_like 'a hash composite'
 end
